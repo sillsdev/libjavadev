a1337ee521f4269ac99d3e2afd4c931e5784c152
Andy Black <blackhandrew@gmail.com>
2024-02-21 14:15:46 -0700

    Generalize keyboard handling; add keyboard changer.

--- src/org/sil/utility/service/keyboards/KeyboardInfo.java
+++ src/org/sil/utility/service/keyboards/KeyboardInfo.java
@@ -1,11 +1,13 @@
 /**
- * Copyright (c) 2020 SIL International
+ * Copyright (c) 2020-2024 SIL International
  * This software is licensed under the LGPL, version 2.1 or later
  * (http://www.gnu.org/licenses/lgpl-2.1.html)
  */
 package org.sil.utility.service.keyboards;
 
 import java.util.Locale;
+
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  * @author Andy Black
@@ -14,22 +16,50 @@ import java.util.Locale;
 public class KeyboardInfo {
 
 	Locale locale;
+	String sLocale;
 	String description;
 	int windowsLangID = 0;
 	
+	public KeyboardInfo() {
+		sLocale = "en";
+		locale = new Locale(sLocale);
+		description = "English";
+		windowsLangID = 0;
+	}
+
 	public KeyboardInfo(Locale locale, String description) {
 		super();
 		this.locale = locale;
+		this.sLocale = locale.getLanguage();
 		this.description = description;
 	}
 	
 	public KeyboardInfo(Locale locale, String description, int windowsLangID) {
 		super();
 		this.locale = locale;
+		if (locale != null) {
+			this.sLocale = locale.getLanguage();
+		}
 		this.description = description;
 		this.windowsLangID = windowsLangID;
 	}
 
+	public KeyboardInfo(String sLocale, String description, int windowsLangID) {
+		this.sLocale = sLocale;
+		this.locale = new Locale(sLocale);
+		this.description = description;
+		this.windowsLangID = windowsLangID;
+	}
+
+	public String getSLocale() {
+		return sLocale;
+	}
+
+	public void setSLocale(String sLocale) {
+		this.sLocale = sLocale;
+	}
+
+	@XmlTransient
 	public Locale getLocale() {
 		return locale;
 	}
